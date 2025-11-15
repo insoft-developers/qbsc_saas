50ab5852ddf3a16a8c7469b8bb98472a5994086d
Insoft Developers <irdn.software@gmail.com>
2025-11-15 16:18:45 +0700

    test cicid

--- resources/views/frontend/footer.blade.php
+++ resources/views/frontend/footer.blade.php
@@ -5,7 +5,7 @@
             <div class="col-12 text-center">
                 <script>
                     document.write(new Date().getFullYear())
-                </script> © QBSC - by <b>Insoft Developers</b>
+                </script> © QBSC Indonesia - by <b>Insoft Developers</b>
             </div>
         </div>
     </div>
