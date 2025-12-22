3880e0f73bbd7dd9429eea46b5e0b90fc33b0fb7
Insoft Developers <irdn.software@gmail.com>
2025-12-22 07:17:03 +0700

    paginate laravel

--- app/Http/Controllers/BOS/BosAbsensiController.php
+++ app/Http/Controllers/BOS/BosAbsensiController.php
@@ -8,27 +8,18 @@ use Illuminate\Http\Request;
 
 class BosAbsensiController extends Controller
 {
-    public function index(Request $request) {
-        
-        $page = $request->query('page', 1);
-        $limit = $request->query('limit', 20);
-
-        $offset = ($page - 1) * $limit;
-        
-        $data = Absensi::with('satpam','company')->where('comid', $request->comid)
-        ->orderBy('id', 'desc')
-        ->offset($offset)
-        ->limit($limit)
-        ->get();
-
-        $total = Absensi::with('satpam','company')->where('comid', $request->comid)->count();
-
+    public function index(Request $request)
+    {
+        $limit = (int) $request->query('limit', 20);
+    
+        $data = Absensi::with(['satpam', 'company'])
+            ->where('comid', $request->comid)
+            ->orderBy('id', 'desc')
+            ->paginate($limit);
+    
         return response()->json([
             "success" => true,
-            "data" => $data,
-            "page" => (int)$page,
-            "total" => $total
+            "data"    => $data
         ]);
-
     }
 }
