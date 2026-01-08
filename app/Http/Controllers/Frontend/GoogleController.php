10b214c4a7ce4f670dbbdfa955c41e5f97b702c4
Insoft Developers <irdn.software@gmail.com>
2026-01-08 23:14:36 +0700

    google re

--- app/Http/Controllers/Frontend/GoogleController.php
+++ app/Http/Controllers/Frontend/GoogleController.php
@@ -30,7 +30,7 @@ class GoogleController extends Controller
             $company = Company::create([
                 'company_name' => uniqid() . '- Company',
                 'is_peternakan' => 99,
-                'referal_code' => session('referral_code')
+                'referal_code' => session('referal_code')
             ]);
 
             $company_id = $company->id;
