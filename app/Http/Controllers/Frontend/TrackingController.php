2a6ea3ce4ba43196abcce216e707becd5f6bd2b2
Insoft Developers <irdn.software@gmail.com>
2026-01-19 00:53:42 +0700

    tracking

--- app/Http/Controllers/Frontend/TrackingController.php
+++ app/Http/Controllers/Frontend/TrackingController.php
@@ -9,6 +9,7 @@ use App\Models\KandangKipas;
 use App\Models\KandangLampu;
 use App\Models\KandangSuhu;
 use App\Models\Patroli;
+use App\Models\SatpamLocation;
 use App\Traits\CommonTrait;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -113,9 +114,6 @@ class TrackingController extends Controller
 
     public function map($id)
     {
-        
-        
-    
         $view = 'map';
 
         $comid = $this->comid();
@@ -131,7 +129,8 @@ class TrackingController extends Controller
         $this->pushRow($row_data, $absensi->jam_masuk, $absensi->satpam->name ?? '', 'Absen Masuk', $absensi->latitude, $absensi->longitude);
 
         // ================= SUHU =================
-        $suhu = KandangSuhu::where('satpam_id', $satpamId)->where('comid', $comid)
+        $suhu = KandangSuhu::where('satpam_id', $satpamId)
+            ->where('comid', $comid)
             ->whereRaw('TIMESTAMP(tanggal, jam) BETWEEN ? AND ?', [$jamMasuk, $jamPulang])
             ->orderByRaw('TIMESTAMP(tanggal, jam)')
             ->get();
@@ -141,7 +140,8 @@ class TrackingController extends Controller
         }
 
         // ================= KIPAS =================
-        $kipas = KandangKipas::where('satpam_id', $satpamId)->where('comid', $comid)
+        $kipas = KandangKipas::where('satpam_id', $satpamId)
+            ->where('comid', $comid)
             ->whereRaw('TIMESTAMP(tanggal, jam) BETWEEN ? AND ?', [$jamMasuk, $jamPulang])
             ->orderByRaw('TIMESTAMP(tanggal, jam)')
             ->get();
@@ -151,7 +151,8 @@ class TrackingController extends Controller
         }
 
         // ================= ALARM =================
-        $alarm = KandangAlarm::where('satpam_id', $satpamId)->where('comid', $comid)
+        $alarm = KandangAlarm::where('satpam_id', $satpamId)
+            ->where('comid', $comid)
             ->whereRaw('TIMESTAMP(tanggal, jam) BETWEEN ? AND ?', [$jamMasuk, $jamPulang])
             ->orderByRaw('TIMESTAMP(tanggal, jam)')
             ->get();
@@ -161,7 +162,8 @@ class TrackingController extends Controller
         }
 
         // ================= LAMPU =================
-        $lampu = KandangLampu::where('satpam_id', $satpamId)->where('comid', $comid)
+        $lampu = KandangLampu::where('satpam_id', $satpamId)
+            ->where('comid', $comid)
             ->whereRaw('TIMESTAMP(tanggal, jam) BETWEEN ? AND ?', [$jamMasuk, $jamPulang])
             ->orderByRaw('TIMESTAMP(tanggal, jam)')
             ->get();
@@ -171,13 +173,23 @@ class TrackingController extends Controller
         }
 
         // ================= PATROLI =================
-        $patroli = Patroli::where('satpam_id', $satpamId)->where('comid', $comid)
+        $patroli = Patroli::where('satpam_id', $satpamId)
+            ->where('comid', $comid)
             ->whereRaw('TIMESTAMP(tanggal, jam) BETWEEN ? AND ?', [$jamMasuk, $jamPulang])
             ->orderByRaw('TIMESTAMP(tanggal, jam)')
             ->get();
 
         foreach ($patroli as $pt) {
             $this->pushRow($row_data, $pt->tanggal . ' ' . $pt->jam, $pt->satpam->name ?? '', 'Patroli ' . ($pt->lokasi->nama_lokasi ?? ''), $pt->latitude, $pt->longitude);
+        }
+
+        $satcol = SatpamLocation::where('satpam_id', $satpamId)
+            ->whereBetween('recorded_at', [$jamMasuk->toDateTimeString(), $jamPulang->toDateTimeString()])
+            ->orderBy('recorded_at', 'asc')
+            ->get();
+
+        foreach($satcol as $sc) {
+            $this->pushRow($row_data, $sc->recorded_at, $sc->satpam->name ?? '', 'Walking ', $sc->latitude, $sc->longitude);
         }
 
         // ================= ABSEN PULANG (FORCE) =================
@@ -193,7 +205,6 @@ class TrackingController extends Controller
 
         // ================= SORT =================
         $row_data = collect($row_data)->sortBy(fn($i) => Carbon::parse($i['tanggal']))->values()->toArray();
-
 
         // return $row_data;
         return view('frontend.aktivitas.tracking.map', compact('view', 'row_data'));
